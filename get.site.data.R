ec09db2a898c421f428a6161db3a4971cf4ad193
Dan Gianotti <gianotti@bu.edu>
2013-12-10 10:29:38 -0500

    Changes to data download/processing. Not ready for prime time.

--- get.site.data.R
+++ get.site.data.R
@@ -1,17 +1,38 @@
 get.site.data <- function(site.number) {
   # The function get.site.data takes an integer input corresponding to the
-  # site numbers in the file SOMETHING1.CSV and creates/updates the GCC and
-  # MODIS data file stored in SOMETHING2.CSV 
+  # sites listed in the file phen_sites.csv and creates/updates the GCC and
+  # MODIS data files for that site. 
   
-  # Check to see if some data has already been downloaded (i.e. if the file
-  # SOMETHING2.CSV exists):
-  some.data.downloaded <- SOME CODE # some.data.downloaded is TRUE/FALSE
+  gcc_filename <- sprintf("gcc_data_site%i,csv",site.number)
+  ndvi_filename <- sprintf("gcc_data_site%i,csv",site.number)
+  
+  # Check to see if some data has already been downloaded (i.e. if the files
+  # already exist):
+  some.data.downloaded <- file.exists(gcc_filename) &&  file.exists(ndvi_filename) # some.data.downloaded is TRUE/FALSE
   
   # If none of the data for that site has been downloaded, then download all
   # of the gcc and MODIS data from 2000-present, and process it. Save the 
   # data in the file SOMETHING2.CSV:
   if(!some.data.downloaded){
-    SOME CODE
+    # Read in the info on where to get phenocam and MODIS data:
+    site.metadata <- read.table("site_metadata.csv",header = TRUE, 
+                                sep=",",stringsAsFactors=FALSE) # site name, phenocam url, lat, lon
+        
+    # Download phenocam data:
+    phenocam.url <- site.metadata$phenocam_url[site.number]
+    sys_command = sprintf("wget -O phenocam_data_site%i.csv %s", site.number, phenocam.url)
+    system(sys_command)
+    
+    # Process phenocam data:
+    
+    # Save gcc data:
+    
+    # Download MODIS data:
+    
+    # Process MODIS data:
+    
+    # Save NDVI data:
+    
   }
   else{ #ie if some.data.downloaded is TRUE, just need to update:
     SOME CODE
