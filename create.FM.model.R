b5969c650811a4d189f48c6d6c757c8c1b5494e5
HollieEmery <hollie@temps-MacBook-Pro.local>
2013-12-10 20:54:48 -0500

    trying to fix conflicts

--- create.FM.model.R
+++ create.FM.model.R
@@ -1,37 +1,43 @@
-particle.filter.FM <- function(site_num,ne,inputs,obs){
+create.FM.model <- function(site_num){
+  ## source("SetEnsembleSize.r") ## if we write a function to make changing the ensemble size easy
+  ne = 10 ## ensemble size
   
-  ########################################
-  #### dummy values for debugging ########
-  ########################################
-  site_num=1
-  ne=100
-  
-  ### r parameter
-  inputs=list()
-  inputs$mean.r=c(0.05, 0.05, 0.05, 0.05, 0.05) # one parameter estimate for each site
-  inputs$sd.r=c(.0001, .0001, .0001, .0001, .0001)
-  
-  ### observation data (real thing should be cleaned, rescaled GCC and NDVI from 2013 only)
-  obs=list()
-  obs$GCC= c(rep(NA,182),cumsum(c(1,rnorm(344-182,-.005,.00001))))
-  obs$NDVI= c(rep(NA,182),cumsum(c(1,rnorm(344-182,-.005,.00002))))
-  obs$NDVI[210:240]=NA #
-  obs$GCC[230:260]=NA #
-  #########################################
-  #########################################
-  
-  source("SSLPM.r") ## Super Simple Logistic Model
-  source("ciEnvelope.R")
-  source("update.r.R")
-  
-  ### Define Initial State
-  if (ne > 1) { ## ne is number of ensemble members
+  ### Define Initial State of ensemble members
+  if (ne > 1) {
     X = rbeta(ne,100,1) ## X is beta distributed very skewed to 1
   }
   X.orig = X
   
+  ### read in output from State Space Model for X and r
+  file_name = paste('Jags.SS.out.site',as.character(site_num), 'RData',sep=".")
+  load(file_name)
+  ### read in phenology data
+  X = as.matrix(jags.out.all.years.array[,5:369,])
+  r = as.vector(jags.out.all.years.array[,1,])
+  ### read in r (growth rate parameter output)
+  
+  
+  ##### for now while historical fit is still in progress:
+  ## mean_r=.05
+  ## sd_r=.0005
+  
+  ## Dummy data for testing
   ### Initial parameters (one for each ensemble member)
-  r=rnorm(ne,inputs$mean.r[site_num],inputs$sd.r[site_num])
+  ## r=rnorm(ne,mean_r,sd_r)
+  
+  ## Super Simple Logistic Phenology Model
+  ## X is phenology state
+  ## r is growth rate parameter in logistic equation
+  SSLPM <- function(X,r) {
+    ne = length(r) ## ne = number of ensemble members
+    # initialize new state
+    Xnew = as.numeric(rep(NA,ne))
+    # update state
+    for(i in 1:ne){
+      Xnew[i] = max(0,min(1,X[i] - r[i] * X[i] * (1-X[i]) ))
+    }
+    return(data.frame(X=Xnew))
+  }
   
   ## Create vector for time to current date
   cur_date = Sys.Date()
@@ -39,70 +45,93 @@ particle.filter.FM <- function(site_num,ne,inputs,obs){
   time = 182:doy
   nt = length(time)
   
-  ## Create a filter with GCC and NDVI equally weighted (uses only one data source if the other is NA)
-  GCC = obs$GCC
-  NDVI = obs$NDVI
-  length = length(GCC)
-  ph.filter=array(NA,(length))
-  for(i in 1:length) {
-    if (!is.na(GCC[i]) & !is.na(NDVI[i])) {
-      val = mean(GCC[i], NDVI[i])
-      ph.filter[i]=val
-    } else if (!is.na(GCC[i]) & is.na(NDVI[i])) {
-      ph.filter[i] = GCC[i] 
-    } else if (is.na(NDVI[i]) & !is.na(NDVI[i])) {
-      ph.filter[i] = NDVI[i] 
-    }
+  #### generate initial ensemble forecast
+  output = array(NA,c(nt,ne))
+  for(t in 1:nt){
+    output[t,]=as.matrix(SSLPM(X,r))
+    X=output[t,]
   }
   
-  ## just the fall dates
-  ph.filter=ph.filter[time] 
+  #plot mean and CI of ensemble members time series
+  source("ciEnvelope.R")
+  ci = apply(output,1,quantile,c(0.025,0.5,0.975))
+  plot(ci[2,],xlab="time",ylab="phenology",type='l')
+  ciEnvelope(1:nt,ci[1,],ci[3,],col="light blue")
+  lines(ci[2,])
   
-  ## not sure how we will error for the particle weights, so I'm using 50% of the mean for now 
-  ph.filter.sd = ph.filter*.5
-                  
+  
+  
+  #### HERE THERE BE DRAGONS ###
+  
+  ## filter with GCC and NDVI (uses only one data source if other has all NA values)
+  for (i = 1:length(GCC)) {
+    if (!is.na(GCC(i))) && (!is.na(NDVI(i))) {
+      NDVI_GCC_filter(i) = 0.5*GCC(i) + 0.5*NDVI(i)}
+    else if (!is.na(GCC(i))) && (is.na(NDVI(i))) {
+      NDVI_GCC_filter(i)= GCC(i) }
+    else if (is.na(NDVI(i))) && (!is.na(NDVI(i))) {
+      NDVI_GCC_filter(i) = NDVI(i) }
+  }
+  
+  ## Calculate model ensemble means for same periods
+  window = rep(1,each=48*1,length=nt)
+  NDVI_GCC_m = t(apply(output[,,2],2,tapply,window,mean))
+  NDVI_GCC_m.ci = apply(NDVI_GCC_m,2,quantile,c(0.025,0.5,0.975))
+  
+  ## plot model and observations
+  Msel = 1:ncol(NDVI_GCC_m.ci)
+  plot(Mtime[Msel],NDVI_GCC_m.ci[2,],ylab="NDVI_GCC",xlab="Time",
+       ylim=range(c(range(NDVI_GCC_m.ci.ci),range(NDVI_GCC_filter,na.rm=TRUE))),type='n')
+  ciEnvelope(Mtime[Msel],NDVI_GCC_m.ci[1,],NDVI_GCC_m.ci[3,],col=col.alpha("lightGrey",0.5))
+  points(Mtime,NDVI_GCC_filter)
+  for(i in 1:length(NDVI_GCC_filter)){
+    lines(rep(Mtime[i],2),NDVI_GCC_filter[i]+c(-1,1)*NDVI_GCC_filter.sd[i])
+  }
+  
   ### resampling particle filter
   sample=0
-  hist.r=list()  ## since we resample parameters, create a record of what values were used each step
-  hist.r[[1]] = r ## initial parameter conditions
-  X = X.orig  ## reset state to the initial values, not the final values from the previous ensemble
-  output = array(NA,c(nt,ne,2)) ## initialize output
-  
-  ###### here's the actual forecast loop
+  hist.params=list() ## since we resample parameters, create a record (history) of what values were used each step
+  hist.params[[1]] = r
+  X = X.orig ## reset state to the initial values, not the final values from the previous ensemble
   for(t in 1:nt){
     
     ## forward step
-    output[t,,]=as.matrix(SSLPM(X,r))
-    X=output[t,,1]
-    r=output[t,,2]
+    output[t,,]=as.matrix(SSSLM(X,r,time[t]))
+    X=output[t,,]
     
     ## analysis step
-    #if(t%%(48*1) == 0){ ## if remainder == 0  ####### this peice is here as a template in case we don't filter every single day
+    ## changed analysis step to 48*1 to be 1/day
+    if(t%%(48*1) == 0){ ## if remainder == 0
       sample = sample+1
       print(sample)
-      if(!is.na(ph.filter[sample])) {  ## if observation is present
+      if(!is.na(NDVI_GCC_filter[sample])){ ## if observation is present
         
         ## calulate Likelihood (weights)
-        Lm = apply(output[t:1, ,1],2,mean) ## model filter over obs period
-        wt = dnorm(ph.filter[sample],Lm,ph.filter.sd[sample])
+        Lm = apply(output[t+1-(48*1):1, ,2],2,mean) ## model NDVI_GCC filter over obs period
+        ## set std for filter to zero for now, make stochastic later
+        NDVI_GCC_filter.sd = 0
+        wt = dnorm(NDVI_GCC_filter[sample],Lm,NDVI_GCC_filter.sd)
         
-        ## resample 
+        ## resample
         index = sample.int(ne,ne,replace=TRUE,prob=wt)
         X = X[index]
-       r = update.r(r,index)    
+        ### still need to define update.r
+        r = update.r(r,index)
       }
-      hist.r[[sample+1]] = r
-    #}
+      hist.params[[sample+1]] = r
+    }
+    
   }
-  ##### end of forecast loop
   
-  ## Extract and summarize ph.filter
-  ph.filter.pr = t(output[,,1])
-  ph.filter.ci  = apply(ph.filter.pr,2,quantile,c(0.025,0.5,0.975))
+  ## Extract and summarize NDVI_GCC FILTER (pr = resampling PF)
+  NDVI_GCC_filter.pr = t(apply(output[,,2],2,tapply,window,mean))
+  NDVI_GCC_filter.ci = apply(NDVI_GCC_filter.pr,2,quantile,c(0.025,0.5,0.975))
   
-  ##plot filter
-  plot(time,ph.filter.ci[2,],type='n',ylab="NDVI_GCC",xlab="Time")
-  ciEnvelope(time,ph.filter.ci[1,],ph.filter.ci[3,],col="light grey")
-  points(time,ph.filter)    
-  
-}  
+  plot(Mtime[Msel],NDVI_GCC_filter.ci[2,],ylim=range(c(range(LAIm.ci),range(NDVI_GCC_filter,na.rm=TRUE))),
+       type='n',ylab="NDVI_GCC",xlab="Time")
+  ciEnvelope(Mtime[Msel],NDVI_GCC_m.ci[1,],NDVI_GCC_m.ci[3,],col=col.alpha("lightGrey",0.5))
+  ciEnvelope(Mtime[Msel],NDVI_GCC_filter.ci[1,],NDVI_GCC_filter.ci[3,],col=col.alpha("lightGreen",0.5))
+  points(Mtime,NDVI_GCC_filter)
+  for(i in 1:length(NDVI_GCC_filter)){
+    lines(rep(Mtime[i],2),NDVI_GCC_filter[i]+c(-1,1)*LAIr.sd[i])
+  }
