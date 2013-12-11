3b30cd4eb27caba928f8236144337d77f4399e92
Josh Mantooth <jam2767@gmail.com>
2013-12-11 14:08:00 -0500

    changed plotting to only be last half of year. SS model output is now only for last half of year. Length of output is now 188.

--- make.SS.plots.R
+++ make.SS.plots.R
@@ -9,7 +9,7 @@ make.SS.plots <- function(jags.out.all.years.array,time,
   pdf(plot_file_name)
   
   count = 0
-  for(YR in 2000:2012){
+  for(YR in 2009:2009){#2000:2012){
     
     count = count+1
     jags.out.one.year = jags.out.all.years.array[,,count]
@@ -19,26 +19,26 @@ make.SS.plots <- function(jags.out.all.years.array,time,
     rescaled_GCC_one_year = rescaled_GCC[II]    # get gcc just for ONE year
     
     # delete leap days 
-    if (length(II) == 366){
-      rescaled_NDVI_one_year = rescaled_NDVI_one_year[1:365]
-      rescaled_GCC_one_year = rescaled_GCC_one_year[1:365]
+    if (length(II) == 185){
+      rescaled_NDVI_one_year = rescaled_NDVI_one_year[182:365]
+      rescaled_GCC_one_year = rescaled_GCC_one_year[182:365]
     }
     
     
     
     # [r  tau_add  tau_gcc	tau_ndvi	x]
-    ci <- apply((jags.out.one.year[,5:369]),2,quantile,c(0.025,0.5,0.975))
+    ci <- apply((jags.out.one.year[,5:ncol(jags.out.matrix)]),2,quantile,c(0.025,0.5,0.975))
     
     # NDVI and GCC
-    plot(1:365,ci[2,],type='l',ylim=c(0, 1),ylab="Rescaled NDVI, GCC")
-    ciEnvelope(1:365,ci[1,],ci[3,],col="lightBlue")
+    plot(182:365,ci[2,],type='l',ylim=c(0, 1),ylab="Rescaled NDVI, GCC")
+    ciEnvelope(182:365,ci[1,],ci[3,],col="lightBlue")
 #    if(!is.null(dim(rescaled_NDVI_one_year))){ # R is stupid with NAs...
-      points(1:365,rescaled_NDVI_one_year,pch="+",cex=0.8)
+      points(182:365,rescaled_NDVI_one_year,pch="+",cex=0.8)
 #    }
 #    if(!is.null(dim(rescaled_GCC_one_year))){ # R is stupid with NAs...
-      points(1:365,rescaled_GCC_one_year,pch="o",cex=0.5)
+      points(182:365,rescaled_GCC_one_year,pch="o",cex=0.5)
 #    }
-    lines(1:365,ci[2,],type='l',ylim=c(0, 1))
+    lines(182:365,ci[2,],type='l',ylim=c(0, 1))
     
     
   }
