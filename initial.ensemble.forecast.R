1f523854ea65bfee0ab315e9e04f4046acf8f384
AngelaRigden <angelarigden@wireless1x-155-41-77-95.bu.edu>
2013-12-13 12:13:18 -0500

    Added an uncertainty plot

--- initial.ensemble.forecast.R
+++ initial.ensemble.forecast.R
@@ -64,7 +64,32 @@ initial.ensemble.FM <- function(site_num){
   ciEnvelope(time,ci[1,],ci[3,],col="light blue")
   lines(time,ci[2,])
   
+  ############## Uncertainty: plot ensemble forecast and observed data so far.
+  # Get data from one site and one year
+  hist_data = read.csv("full_historical_data.csv")
+  site_data = subset(hist_data,hist_data$site_ID == site_num)
+  time2=site_data$date
+  time_year = as.numeric(format(as.Date(time2), "%Y"))
+
+  max_NDVI <- max(site_data$NDVI,na.rm=TRUE)
+  min_NDVI <- min(site_data$NDVI,na.rm=TRUE)
+  max_GCC <- max(site_data$GCC,na.rm=TRUE)
+  min_GCC <- min(site_data$GCC,na.rm=TRUE)
+  rescaled_NDVI <- (site_data$NDVI-min_NDVI)/(max_NDVI-min_NDVI)
+  rescaled_GCC <- (site_data$GCC-min_GCC)/(max_GCC-min_GCC)
+  
+  II = which(time_year==2013)
+  gcc2013 = rescaled_GCC[II]
+  ndvi2013 = rescaled_NDVI[II]
+  
+  ci = apply(output[,,1],1,quantile,c(0.025,0.5,0.975),na.rm=TRUE)
+  plot(time,ci[2,],ylim=c(0,1),xlab="time",ylab="X",type='l')
+  ciEnvelope(time,ci[1,],ci[3,],col="light blue")
+  points(global_input_parameters$model.start.DOY:365,gcc2013[global_input_parameters$model.start.DOY:365],pch="+",cex=0.8)
+  points(global_input_parameters$model.start.DOY:365,ndvi2013[global_input_parameters$model.start.DOY:365],pch="o",cex=0.8)
+  lines(time,ci[2,])
+
   ############## Sensitivity
-  ############## Uncertainty
+  # Since there is no environmental/climate driver, what are we taking the sensitivity of?
   
 }
