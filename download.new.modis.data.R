941efcde3f387fce74607481cff4efe95a8cee68
Dan Gianotti <gianotti@bu.edu>
2013-12-13 00:48:16 -0500

    Hopefully done with get.site.data and all internal functions

---
+++ download.new.modis.data.R
@@ -0,0 +1,74 @@
+download.new.modis.data <- function(site.number) {
+  # The function download.new.modis.data looks for the last modis data for the site 
+  # site.number, and then downloads any data that comes after that date.
+  
+  ndvi_filename <- sprintf("ndvi_data_site%i.csv",site.number)
+  
+  old.ndvi.data <- read.csv(ndvi_filename)
+  
+  # Find the last non-NA data point in the file
+  not.nas <- !is.na(old.ndvi.data$ndvi)
+  counter <- 1:length(not.nas)
+  last.data.index <- max(counter[not.nas])
+  last.data.date <- as.Date(old.ndvi.data$date[last.data.index])
+  
+  # If the last data point isn't from today...
+  if(last.data.date < Sys.Date()) { 
+    # Then we download the modis data:
+    
+    # Load some required packages:
+    loaded <- require('devtools')
+    if(!loaded){
+      print("trying to install devtools")
+      install.packages("devtools")
+      loaded <- require('devtools')
+      if(loaded){
+        print("devtools installed and loaded")
+      } 
+      else {
+        stop("could not install devtools")
+      }    
+    }
+    
+    loaded <- require('MODISTools')
+    if(!loaded){
+      print("trying to install MODISTools")
+      install.packages("MODISTools")
+      loaded <- require('MODISTools')
+      if(loaded){
+        print("MODISTools installed and loaded")
+      } 
+      else {
+        stop("could not install MODISTools")
+      }    
+    }
+    
+    
+    # Read in the info on where to get phenocam and MODIS data:
+    site.metadata <- read.table("site_metadata.csv",header = TRUE, 
+                                sep=",",stringsAsFactors=FALSE) # site name, phenocam url, lat, lon
+    
+    site.lat <- site.metadata$lat[site.number]
+    site.lon <- site.metadata$lon[site.number]
+    
+    first.year <- format(last.data.date, "%Y")
+    last.year <-  format(Sys.Date(), "%Y")
+    
+    # Download MODIS data
+    print(sprintf("Downloading MODIS data for site %i.\n",site.number))
+    MODISSubsets(data.frame(lat = site.lat,
+                            long = site.lon,
+                            start.date = first.year,
+                            end.date = last.year),
+                 Product="MOD09A1",Bands=c("sur_refl_day_of_year","sur_refl_qc_500m",
+                                           "sur_refl_state_500m","sur_refl_vzen",
+                                           "sur_refl_b01","sur_refl_b02"),
+                 Size=c(1,1), SaveDir = ".", StartDate=TRUE)
+    
+  }
+  else{ # No new data to download...
+    return()
+  }
+  
+}
+
---
+++ update.ndvi.data.R
@@ -0,0 +1,104 @@
+update.ndvi.data <- function(site.number){
+  # The function update.ndvi.data loads the new MODIS data for the site site.number
+  # and calculates the NDVI values for that site. Output is appended into 
+  # ndvi_data_siteX.csv, where X is the site number
+  
+  # First, we need to know the file name of the MODIS data that we just downloaded.
+  # Look in the "Subset Download..." file:
+  subset.filename <- Sys.glob("Subset Download*") # The file that says where the MODIS data was just saved
+  
+  # Load that file...
+  file.info <- read.csv(subset.filename) 
+  
+  # Find the filename of the modis data file:
+  modis.filename <- paste(as.character(file.info$SubsetID[1]),"_MOD09A1.asc",sep="")
+  
+  # Load the modis data:
+  modis_dat = read.csv(modis.filename,header=FALSE)
+  
+  # find the unique ID number. There is a unique ID number associated with every 
+  # day (shared by all vars for that day)
+  id_num <- unique(modis_dat$V5)
+  id_num <- id_num[!is.na(id_num)]
+  year_dat = as.numeric(substr(id_num,1,4)) # find the year associated with that day
+  
+  # Initialize arrays
+  band_1_data= rep(NA,length(id_num))
+  band_2_data = rep(NA,length(id_num))
+  DOY_data = rep(NA,length(id_num))
+  date_format = as.Date(rep(NA,length(id_num)))
+  site_ID = rep(i,length(id_num)) # Just for ease, assign ID number to each site (1:5, rows of phen_sites.csv)
+  
+  for (p in 1:length(id_num) ) { # for loop over each day.  
+    
+    day_index = which(id_num[p]==modis_dat$V5) # for each ID number, find the rows that match that ID number (i.e. from the same day)
+    day_data = modis_dat[as.numeric(day_index),] # extract the modis data for that index (that day)
+    
+    band_1_index = grep('b01',day_data$V1) # get index band 1
+    band_2_index = grep('b02',day_data$V1) # get index band 2
+    DOY_index = grep('day_of_year',day_data$V1) # get index DOY
+    
+    band_1_data[p] = mean(as.numeric(day_data[band_1_index,6:ncol(day_data)])) # Average over all pixels
+    band_2_data[p] = mean(as.numeric(day_data[band_2_index,6:ncol(day_data)])) # Average over all pixels
+    DOY_data[p] = day_data[DOY_index,6] 
+    
+    date_info = paste(year_dat[p],DOY_data[p]) # Convert DOY, year to date
+    date_format[p] = strptime(date_info, "%Y %j")
+  }
+  
+  # Calculate the NDVI!
+  NDVI_cal = (band_2_data - band_1_data) / (band_2_data + band_1_data)
+  
+  MODIS_DATA_ST <- data.frame(date = date_format, ndvi = NDVI_cal)
+  
+  # Need to delete leap days...
+  leap_days <- is.na(MODIS_DATA_ST)
+  MODIS_DATA_ST <- as.data.frame(subset.data.frame(MODIS_DATA_ST,!leap_days[,2]))
+  # leap days deleted!      
+  
+  # Also delete any dates that are NA (why do these exist??)
+  MODIS_DATA_ST <- as.data.frame(subset.data.frame(MODIS_DATA_ST,!leap_days[,1]))
+  
+  
+  # Create a vector of possible data observation dates
+  source("global_input_parameters.R")
+  start.date <- as.Date(global_input_parameters$data.start.date)
+  current.year <- format(Sys.time(), "%Y")
+  end.date <- as.Date(paste(current.year,"-12-31",sep=""))
+  daily.dates = seq(start.date, end.date, by="days")
+  
+  # Finds indices of dates of modis data that are observed (and match possible_days)
+  days.with.modis.data = match(as.Date(MODIS_DATA_ST$date),daily.dates)
+  
+  # make time series vector of new modis data, the same length as daily.dates
+  ndvi.new = rep(NA,length(daily.dates))
+  ndvi.new[days.with.modis.data] = MODIS_DATA_ST$ndvi
+  
+  # Load the existing NDVI data:
+  old.ndvi.data <- read.csv(sprintf("ndvi_data_site%i.csv",site.number))
+
+  # Get the last date in the old data which is not NA:
+  not.nas <- !is.na(old.ndvi.data$ndvi)
+  counter <- 1:length(not.nas)
+  last.data.index <- max(counter[not.nas])
+  last.data.date <- as.Date(old.ndvi.data$date[last.data.index])
+  
+  # Get the index in the new data that corresponds to that date:
+  matches.date <- (daily.dates == last.data.date) # All FALSE except 1 TRUE
+  counter <- 1:length(daily.dates)
+  new.data.index <- counter[matches.date] # should just be a single integer
+  
+  # Make a data frame of the new data:
+  new.ndvi.data <- data.frame(date = daily.dates[(new.data.index+1):length(daily.data)],
+                              ndvi = ndvi.new[(new.data.index+1):length(ndvi.new)])
+  
+  # Stick the old date (through last.data.date) together with the new data (after
+  # last.data.date):
+  combined.data = rbind(old.ndvi.data[1:last.data.index,],new.ndvi.data)
+  
+  # Save NDVI data:
+  write.csv(combined.data, file = sprintf("ndvi_data_site%i.csv",site.number),row.names=FALSE)
+  
+  
+  
+}
