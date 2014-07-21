1e4babc9f84bb8a7e531961521ef1c7928bc29be
Mike Dietze <dietze@bu.edu>
2014-07-21 12:28:25 -0400

    MODISTools R library does not appear to be functioning so switched code to use the PEcAn library, data.remote, for downloading MODIS (which calls a Python script). This code saves the data in a different format (netCDF, one file per band) but it also returns the data as an R matrix, so this is also being saved as an RData object. subsequent code is being modified to use that RData object. Added a boolean variable, useMODISTools, to the global_input_parameters, but at the moment backward compatibility is not functional.  Need to update download.new.modis.data to use the same library and data format

--- download.all.modis.data.R
+++ download.all.modis.data.R
@@ -1,6 +1,10 @@
 download.all.modis.data <- function(site.number) {
   # Downloads all of the modis data for the site site.number
   
+  source("global_input_parameters.R") # For burn-in
+  model = global_input_parameters$model
+  useMODISTools = global_input_parameters$useMODISTools
+  
   # Load some required packages:
   loaded <- require('devtools')
   if(!loaded){
@@ -28,6 +32,7 @@ download.all.modis.data <- function(site.number) {
     }    
   }
   
+  havePEcAn = require('PEcAn.data.remote')
   
   # Read in the info on where to get phenocam and MODIS data:
   site.metadata <- read.table("site_metadata.csv",header = TRUE, 
@@ -45,9 +50,13 @@ download.all.modis.data <- function(site.number) {
   first.year.with.data = as.numeric(substr(start.date.string,2,5)) # Gets year of first date 
   last.year.with.data = as.numeric(substr(end.date.string,2,5)) # Gets year of last date 
   
+  start.date = as.integer(substr(start.date.string,2,8))
+  end.date = as.integer(substr(end.date.string,2,8))
+  
   # Download MODIS data
   print(sprintf("Downloading MODIS data for site %i.\n",site.number))
-  MODISSubsets(data.frame(lat = site.lat,
+  if(useMODISTools){
+    MODISSubsets(data.frame(lat = site.lat,
                           long = site.lon,
                           start.date = first.year.with.data,
                           end.date = last.year.with.data),
@@ -55,6 +64,32 @@ download.all.modis.data <- function(site.number) {
                                          "sur_refl_state_500m","sur_refl_vzen",
                                          "sur_refl_b01","sur_refl_b02"),
                Size=c(1,1), SaveDir = ".", StartDate=TRUE)
+  }else{
+    if(havePEcAn){
+      doy = call_MODIS("./MODIS",paste0(site.number,".DOY.nc"),start = start.date,end = end.date,
+                 lat = site.lat,lon=site.lon,product="MOD09A1",band="sur_refl_day_of_year")
+
+      qc = call_MODIS("./MODIS",paste0(site.number,".qc.nc"),start = start.date,end = end.date,
+                         lat = site.lat,lon=site.lon,product="MOD09A1",band="sur_refl_qc_500m")
+      
+      state = call_MODIS("./MODIS",paste0(site.number,".state.nc"),start = start.date,end = end.date,
+                       lat = site.lat,lon=site.lon,product="MOD09A1",band="sur_refl_state_500m")
+
+      vzen = call_MODIS("./MODIS",paste0(site.number,".vzen.nc"),start = start.date,end = end.date,
+                         lat = site.lat,lon=site.lon,product="MOD09A1",band="sur_refl_vzen")
+      
+      b01 = call_MODIS("./MODIS",paste0(site.number,".b01.nc"),start = start.date,end = end.date,
+                        lat = site.lat,lon=site.lon,product="MOD09A1",band="sur_refl_b01")
+
+      b02 = call_MODIS("./MODIS",paste0(site.number,".b02.nc"),start = start.date,end = end.date,
+                       lat = site.lat,lon=site.lon,product="MOD09A1",band="sur_refl_b02")
+      
+      site.data = list(date=doy$date,qc=unlist(qc$m),doy = unlist(doy$m),state = unlist(state$m),vzen=unlist(vzen$m),b01=unlist(b01$m),b02=unlist(b02$m))
+      
+      save(site.data,file=paste0("./MODIS/",site.number,".RData"))
+      
+    }
+  }
   
     
 }
