6ebc06e1c43d455e02151f98006fe31f167064c0
Mike Dietze <dietze@bu.edu>
2014-07-22 21:53:41 -0500

    Modified MakeAnimation in order to specify Model and Year as command line args

--- MakeAnimation.sh
+++ MakeAnimation.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # Make gif animation out of plots:
+## arg 1 = model
+## arg 2 = year
 
 cd pdfs;
 
 for d in site*
 do cd "$d";
 
-for f in *.pdf; 
+for f in *${1}*${2}*.pdf; 
 do 
 convert ./"$f" ./"${f%.pdf}.jpg"; 
 done
 
-convert -delay 15 *.jpg -loop 0 Site"${d%.pdf}.gif";
+convert -delay 15 *.jpg -loop 0 Site"${d%.pdf}.${1}.${2}.gif";
 rm *.jpg;
 cd ..;
 echo "Finished another site"
